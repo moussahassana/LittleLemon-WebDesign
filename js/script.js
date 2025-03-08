a831f549aa347f0a79baae7591318cb68848d1c6
Moussa Hassana <moussahassana42@gmail.com>
2025-03-08 23:39:26 +0100

    feat: improve responsive design & enhance hamburger menu animation

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const hamburger = document.getElementById('hamburger');
     const navMenu = document.getElementById('nav-menu');
 
-    hamburger.addEventListener('click', function() {
-        navMenu.classList.toggle('active');
+    hamburger.addEventListener('click', function () {
         hamburger.classList.toggle('active');
+        navMenu.classList.toggle('active');
     });
 });
